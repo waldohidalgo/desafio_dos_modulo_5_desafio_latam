87cfaa7c71e81d54efc3a8954af48e2940bb60c6
Waldo Hidalgo <whidalgohp@gmail.com>
2024-03-04 20:35:34 -0300

    🎉 upload full challenge

---
+++ README.md
@@ -0,0 +1,62 @@
+# Contenido
+
+El presente repositorio contiene el código sql solución al desafío número 2 del módulo 5 llamado **Fundamentos de bases de datos relaciones**. Los requisitos del desafío evaluado los muestro en la siguiente imagen:
+
+![Requisitos Hoja Uno y Dos](./screenshots/requisitos_uno_dos.jpg)
+![Requisitos Hojas Tres](./screenshots/requisitos_tres.webp)
+
+## Soluciones
+
+### 1. ¿Cuántos registros hay?
+
+#### 1.1-Código
+
+![Codigo ejercicio 1](./screenshots/solucion_ejercicio_1.png)
+
+#### 1.2-Output
+
+![Output ejercicio 1](./screenshots/output_ejercicios_uno.jpg)
+
+### 2. ¿Cuántos inscritos hay en total?
+
+#### 2.1-Código
+
+![Codigo ejercicio 2](./screenshots/solucion_ejercicio_2.png)
+
+#### 2.2-Output
+
+![Output ejercicio 2](./screenshots/output_ejercicios_dos.jpg)
+
+### 3. ¿Cuál o cuáles son los registros de mayor antigüedad?
+
+#### 3.1-Código
+
+![Codigo ejercicio 3](./screenshots/solucion_ejercicio_3.png)
+
+#### 3.2-Output
+
+![Output ejercicio 3](./screenshots/output_ejercicios_tres.jpg)
+
+### 4. ¿Cuántos inscritos hay por día? (entendiendo un día como una fecha distinta de ahora en adelante)
+
+#### 4.1-Código
+
+![Codigo ejercicio 4](./screenshots/solucion_ejercicio_4.png)
+
+#### 4.2-Output
+
+![Output ejercicio 4](./screenshots/output_ejercicios_cuatro.jpg)
+
+### 5. ¿Qué día se inscribieron la mayor cantidad de personas y cuántas personas se inscribieron en ese día?
+
+#### 5.1-Código
+
+![Codigo ejercicio 5](./screenshots/solucion_ejercicio_5.png)
+
+#### 5.2-Output
+
+![Output ejercicio 5](./screenshots/output_ejercicios_cinco.jpg)
+
+##### Forma 2
+
+![Codigo ejercicio 5 Forma 2](./screenshots/solucion_ejercicio_5_forma_2.png)
---
+++ script_queries.sql
@@ -0,0 +1,188 @@
+CREATE TABLE
+    INSCRITOS (cantidad INT, fecha DATE, fuente VARCHAR);
+
+BEGIN;
+
+INSERT INTO
+    INSCRITOS (cantidad, fecha, fuente)
+VALUES
+    (44, '01/01/2021', 'Blog');
+
+INSERT INTO
+    INSCRITOS (cantidad, fecha, fuente)
+VALUES
+    (56, '01/01/2021', 'Página');
+
+INSERT INTO
+    INSCRITOS (cantidad, fecha, fuente)
+VALUES
+    (39, '01/02/2021', 'Blog');
+
+INSERT INTO
+    INSCRITOS (cantidad, fecha, fuente)
+VALUES
+    (81, '01/02/2021', 'Página');
+
+INSERT INTO
+    INSCRITOS (cantidad, fecha, fuente)
+VALUES
+    (12, '01/03/2021', 'Blog');
+
+INSERT INTO
+    INSCRITOS (cantidad, fecha, fuente)
+VALUES
+    (91, '01/03/2021', 'Página');
+
+INSERT INTO
+    INSCRITOS (cantidad, fecha, fuente)
+VALUES
+    (48, '01/04/2021', 'Blog');
+
+INSERT INTO
+    INSCRITOS (cantidad, fecha, fuente)
+VALUES
+    (45, '01/04/2021', 'Página');
+
+INSERT INTO
+    INSCRITOS (cantidad, fecha, fuente)
+VALUES
+    (55, '01/05/2021', 'Blog');
+
+INSERT INTO
+    INSCRITOS (cantidad, fecha, fuente)
+VALUES
+    (33, '01/05/2021', 'Página');
+
+INSERT INTO
+    INSCRITOS (cantidad, fecha, fuente)
+VALUES
+    (18, '01/06/2021', 'Blog');
+
+INSERT INTO
+    INSCRITOS (cantidad, fecha, fuente)
+VALUES
+    (12, '01/06/2021', 'Página');
+
+INSERT INTO
+    INSCRITOS (cantidad, fecha, fuente)
+VALUES
+    (34, '01/07/2021', 'Blog');
+
+INSERT INTO
+    INSCRITOS (cantidad, fecha, fuente)
+VALUES
+    (24, '01/07/2021', 'Página');
+
+INSERT INTO
+    INSCRITOS (cantidad, fecha, fuente)
+VALUES
+    (83, '01/08/2021', 'Blog');
+
+INSERT INTO
+    INSCRITOS (cantidad, fecha, fuente)
+VALUES
+    (99, '01/08/2021', 'Página');
+
+COMMIT;
+
+--1. ¿Cuántos registros hay?
+SELECT
+    COUNT(*)
+FROM
+    inscritos;
+
+--2. ¿Cuántos inscritos hay en total?
+SELECT
+    SUM(cantidad) AS total_inscritos
+FROM
+    inscritos;
+
+--3. ¿Cuál o cuáles son los registros de mayor antigüedad?
+SELECT
+    *
+FROM
+    inscritos
+WHERE
+    fecha IN (
+        SELECT
+            MIN(fecha)
+        FROM
+            inscritos
+    );
+
+/*
+4. ¿Cuántos inscritos hay por día? (entendiendo un día como una fecha distinta de
+ahora en adelante)
+ */
+SELECT
+    fecha,
+    COUNT(*)
+FROM
+    inscritos
+GROUP BY
+    fecha
+ORDER BY
+    fecha;
+
+/*
+5. ¿Qué día se inscribieron la mayor cantidad de personas y cuántas personas se
+inscribieron en ese día?
+ */
+SELECT
+    fecha,
+    inscritos_por_dia
+FROM
+    (
+        SELECT
+            fecha,
+            SUM(cantidad) AS inscritos_por_dia
+        FROM
+            inscritos
+        GROUP BY
+            fecha
+        ORDER BY
+            fecha
+    ) agrupacion
+WHERE
+    inscritos_por_dia = (
+        SELECT
+            MAX(inscritos_por_dia)
+        FROM
+            (
+                SELECT
+                    fecha,
+                    SUM(cantidad) AS inscritos_por_dia
+                FROM
+                    inscritos
+                GROUP BY
+                    fecha
+                ORDER BY
+                    fecha
+            ) AS agrupacion
+    );
+
+--Forma 2:
+SELECT
+    fecha,
+    SUM(cantidad) AS inscritos_por_dia
+FROM
+    inscritos
+GROUP BY
+    fecha
+HAVING
+    SUM(cantidad) = (
+        SELECT
+            MAX(inscritos_por_dia)
+        FROM
+            (
+                SELECT
+                    fecha,
+                    SUM(cantidad) AS inscritos_por_dia
+                FROM
+                    inscritos
+                GROUP BY
+                    fecha
+                ORDER BY
+                    fecha
+            ) AS agrupado
+    );
